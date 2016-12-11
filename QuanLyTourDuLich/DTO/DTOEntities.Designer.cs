92e8405971f45bd278f6c85be7832c31f6e7e194
7ung <hohoangtung12a3@gmail.com>
2016-12-11 16:25:30 +0700

    Thêm DTO

---
+++ QuanLyTourDuLich/DTO/DTOEntities.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'D:\7ung doc\7th Simeter\OOD2\QuanLyTourDuLich\QuanLyTourDuLich\DTO\DTOEntities.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
