bc7572addafd0046108ba02baabc7713bc7c339d
huynphu <phuhnt@gmail.com>
2016-12-18 19:11:22 +0700

    Xong đăng nhập + đăng xuất + phân quyền

--- QuanLyTourDuLich/Properties/Resources.Designer.cs
+++ QuanLyTourDuLich/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace QuanLyTourDuLich.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap accountmanager {
+            get {
+                object obj = ResourceManager.GetObject("accountmanager", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap add_circular_outlined_button {
             get {
                 object obj = ResourceManager.GetObject("add-circular-outlined-button", resourceCulture);
